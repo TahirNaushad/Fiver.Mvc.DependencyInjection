4e0c953ea659dc13aa11d07315efa864f39fa9da
Tahir Naushasd <t.naushad25@gmail.com>
2017-06-25 01:16:39 +0100

    setup

---
+++ Fiver.Mvc.DependencyInjection/Models/Home/LookupService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiver.Mvc.DependencyInjection.Models.Home
+{
+    public interface ILookupService
+    {
+        List<SelectListItem> Genres { get; }
+    }
+
+    public class LookupService : ILookupService
+    {
+        public List<SelectListItem> Genres
+        {
+            get
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "0", Text = "Thriller" },
+                    new SelectListItem { Value = "1", Text = "Comedy" },
+                    new SelectListItem { Value = "2", Text = "Drama" },
+                    new SelectListItem { Value = "3", Text = "Romance" },
+                };
+            }
+        }
+    }
+}
